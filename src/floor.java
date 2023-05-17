63744669aa225c8eb189eaa4f397b0036ac30d18
Satyam <tsingh30399@gmail.com>
2023-05-18 04:00:04 +0530

    ceiling

--- src/floor.java
+++ src/floor.java
@@ -1,7 +1,7 @@
 public class floor {
     public static void main(String[] args) {
         int[] arr ={2, 3, 5, 9, 14, 16, 18};
-        int target = 5;
+        int target = 1;
         int ans = binarySearch(arr, target);
         System.out.println(ans);
     }
