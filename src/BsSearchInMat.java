772f864e5487440c53655f92a96ed5fa2695db1e
Satyam <tsingh30399@gmail.com>
2023-06-12 04:18:35 +0530

    BS in 2-D Array

---
+++ src/BsSearchInMat.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class BsSearchInMat {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {28, 29, 37, 49},
+                {33, 34, 38, 50}
+        };
+        int target = 37;
+        System.out.println(Arrays.toString(Search(arr, target)));
+
+    }
+    static int[] Search(int[][] arr, int target){
+        int r = 0;
+        int c = arr.length - 1;
+        while (r < arr.length && c >= 0){
+            if(arr[r][c] == target){
+                return new int[]{r, c};
+            }
+            else if(arr[r][c] < target){
+                r++;
+            } else {
+                c--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
