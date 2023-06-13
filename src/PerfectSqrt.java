0007adfaa93d0f84c4d33236059bda16af796627
Satyam <tsingh30399@gmail.com>
2023-06-14 03:46:51 +0530

    Table

---
+++ src/PerfectSqrt.java
@@ -0,0 +1,10 @@
+public class PerfectSqrt {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(String.format("%d  *  %d  =  %d ",n,i,n*i));
+        }
+
+    }
+
+}
