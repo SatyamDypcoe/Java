8b93e6ddf5297f8fb2622a5c3ee7017bb4eb6788
Satyam <tsingh30399@gmail.com>
2023-06-19 03:11:04 +0530

    diff of even-max nd odd-min

---
+++ src/DiffInMaxMin.java
@@ -0,0 +1,24 @@
+//You are given an array of integers A of size N.
+//Return the difference between the maximum among all even numbers of A and the minimum among all odd numbers in A.
+public class DiffInMaxMin {
+    public static void main(String[] args) {
+    int[] A = {-98, 54, -52, 15, 23, -97, 12, -64, 52,85};
+        System.out.println(solve(A));
+    }
+    public static int solve(int[] A) {
+        int emax = Integer.MIN_VALUE;
+        int omax = Integer.MAX_VALUE;
+        for(int i = 0; i< A.length; i++ ){
+            if((A[i] % 2 == 0) && (A[i] > emax)){
+                emax = A[i];
+            }
+            else if(((A[i] % 2) !=0) && (A[i] <=omax)){
+                omax = A[i];
+            }
+        }
+
+
+        // System.out.println(-97%2);
+        return emax - omax;
+    }
+}
