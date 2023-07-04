2c1a1d9aeb4ce7a96c113605e12b2c4de97723e4
Satyam <tsingh30399@gmail.com>
2023-07-04 23:48:50 +0530

    Counting Sub Array

---
+++ src/CountingSubarr.java
@@ -0,0 +1,40 @@
+//Given an array A of N non-negative numbers and a non-negative number B,
+//you need to find the number of subarrays in A with a sum less than B.
+//We may assume that there is no overflow.
+//Example Input
+//Input 1:
+// A = [2, 5, 6]
+// B = 10
+//Input 2:
+// A = [1, 11, 2, 3, 15]
+// B = 10
+//Example Output
+//Output 1:
+// 4
+//Output 2:
+// 4
+//Explanation 1:
+// The subarrays with sum less than B are {2}, {5}, {6} and {2, 5},
+//Explanation 2:
+// The subarrays with sum less than B are {1}, {2}, {3} and {2, 3}
+public class CountingSubarr {
+    public static void main(String[] args) {
+        int[] A ={2, 5, 6};
+        int B = 10;
+        System.out.println(solve(A, B));
+    }
+    public static int solve(int[] A, int B) {
+        int n = A.length;
+        int count = 0;
+        for(int i = 0; i < n; i++ ){
+            int sum = 0;
+            for(int j = i; j<n; j++){
+                sum = sum + A[j];
+                if(sum < B){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
