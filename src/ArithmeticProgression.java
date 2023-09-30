a98189c7265383c8648ee845f112a8c5c910c6eb
Satyam <tsingh30399@gmail.com>
2023-09-30 16:24:18 +0530

    Arithmetic Progression

--- src/ArithmeticProgression.java
+++ src/ArithmeticProgression.java
@@ -2,24 +2,16 @@
 //
 //A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
 //
-//
-//
 //Problem Constraints
 //2 <= N <= 105
 //
 //-109 <= A[i] <= 109
 //
-//
-//
 //Input Format
 //The first and only argument is an integer array A of size N.
 //
-//
-//
 //Output Format
 //Return 1 if the array can be rearranged to form an arithmetic progression, otherwise return 0.
-//
-//
 //
 //Example Input
 //Input 1:
@@ -29,7 +21,6 @@
 //
 // A = [2, 4, 1]
 //
-//
 //Example Output
 //Output 1:
 //
@@ -37,7 +28,6 @@
 //Output 2:
 //
 // 0
-//
 //
 //Example Explanation
 //Explanation 1:
