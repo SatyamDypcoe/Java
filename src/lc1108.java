3f410ddf7479f398b33e19acd2d812fcb0811208
Satyam <tsingh30399@gmail.com>
2023-10-08 20:06:25 +0530

    Defanging an IP Address

---
+++ src/lc1108.java
@@ -0,0 +1,18 @@
+public class lc1108 {
+    public static void main(String[] args) {
+        String address = "255.100.50.0";
+        System.out.println(defangIPaddr(address));
+    }
+    public static String defangIPaddr(String address) {
+        int n = address.length();
+        String ans = "";
+        for(int i =0; i < n; i++){
+            if(address.charAt(i) == '.'){
+                ans = ans + "[.]";
+            }else{
+                ans = ans + address.charAt(i);
+            }
+        }
+        return ans;
+    }
+}
