8444177d1d5c4d3bae69af1a5b384d126505aabd
Satyam <tsingh30399@gmail.com>
2023-10-12 15:39:37 +0530

    Merge Sorted Array

---
+++ src/Lc88.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Lc88 {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+        System.out.println(Arrays.toString(merge(nums1, m, nums2, n)));
+    }
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m- 1;
+        int j = n-1;
+        int k = nums1.length - 1;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+                k--;
+            }else{
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
+        }
+        while(i >= 0){
+            nums1[k] = nums1[i];
+            i--;
+            k--;
+        }
+        while(j >= 0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+        return nums1;
+    }
+}
